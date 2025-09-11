e7aa3be643b13d66196294f97f8fbe42a8b167d7
Patricia <patriciaanneu@gmail.com>
2025-09-10 23:04:31 -0400

    Tasks

---
+++ Assignment1_PatriciaUsal_F2025/src/com/pu/main/assign1/DriverMain.java
@@ -0,0 +1,31 @@
+package com.pu.main.assign1;
+
+/**
+ * @author Patricia Usal
+ * @since 2025-09-10
+ */
+
+import com.pu.grades.assign1.GradeCalculation;
+import com.pu.conversion.assign1.ConvertMilesToKilometers;
+import com.pu.conversion.assign1.ConvertLitersToGallons;
+import com.pu.currency.assign1.CurrencyConversion;
+
+import java.util.Scanner;
+
+public class DriverMain {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		GradeCalculation gradeCalculation = new GradeCalculation();
+		ConvertMilesToKilometers milesToKm = new ConvertMilesToKilometers();
+		ConvertLitersToGallons litersToGallons = new ConvertLitersToGallons();
+		CurrencyConversion currencyConversion = new CurrencyConversion();
+		
+		
+		
+
+		
+	}
+
+}
